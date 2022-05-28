e2702d37db918672fef6a5b1608472c53188fbd5
HenryDevMaster <100979680+Henry12960@users.noreply.github.com>
2022-05-28 12:54:03 -0300

    Add files via upload

---
+++ src/Henrydm/Ping38/Ping38.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Henrydm\Ping38;
+
+use pocketmine\plugin\PluginBase;
+
+class Ping38 extends PluginBase{
+	
+	protected static $instance;
+	
+	public function onEnable():void{
+		self::$instance=$this;
+		@mkdir($this->getDataFolder());
+		$this->saveDefaultConfig();
+		$this->getScheduler()->scheduleRepeatingTask(new PingTagTask, 10);
+	}
+	public static function getInstance():self{
+		return self::$instance;
+	}
+}
---
+++ src/Henrydm/Ping38/PingTagTask.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Henrydm\Ping38;
+
+use pocketmine\scheduler\Task;
+
+class PingTagTask extends Task{
+	
+	public function onRun(int $tick):void{
+		foreach(PingTag::getInstance()->getServer()->getOnlinePlayers() as $player){
+			$tag=PingTag::getInstance()->getConfig()->get("Format");
+			$tag=str_replace("{ping}", $player->getPing(), $tag);
+			$player->setScoreTag($tag);
+		}
+	}
+}
