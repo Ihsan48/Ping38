49fbe05a0299ee909eccf60310cb403069de439f
HenryDevMaster <100979680+Henry12960@users.noreply.github.com>
2022-05-28 12:57:52 -0300

    Fix Display name

--- src/Henrydm/Ping38/PingTagTask.php
+++ src/Henrydm/Ping38/PingTagTask.php
@@ -6,7 +6,7 @@ use pocketmine\scheduler\Task;
 
 class PingTagTask extends Task{
 	
-	public function onRun(int $tick):void{
+	public function onRun(int $tick): void {
 		foreach(PingTag::getInstance()->getServer()->getOnlinePlayers() as $player){
 			$tag=PingTag::getInstance()->getConfig()->get("Format");
 			$tag=str_replace("{ping}", $player->getPing(), $tag);
