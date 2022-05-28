6e58c095a8de7fab5bdca4c59adc3d924799e045
HenryDevMaster <100979680+Henry12960@users.noreply.github.com>
2022-05-28 13:03:52 -0300

    Update

--- src/Henrydm/Ping38/PingTagTask.php
+++ src/Henrydm/Ping38/PingTagTask.php
@@ -8,7 +8,7 @@ class PingTagTask extends Task{
 	
 	public function onRun() : void {
 		foreach(PingTag::getInstance()->getServer()->getOnlinePlayers() as $player){
-			$tag=PingTag::getInstance()->getConfig()->get("Format");
+			$tag=PingTag::getInstance(int $tick)->getConfig()->get("Format");
 			$tag=str_replace("{ping}", $player->getPing(), $tag);
 			$player->setScoreTag($tag);
 		}
