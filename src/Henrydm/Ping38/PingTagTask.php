3e00fcb498e613121a84038eef5e78ab6cf1c770
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-05-31 14:33:53 +0700

    Fix & Clean

--- src/Henrydm/Ping38/PingTagTask.php
+++ src/Henrydm/Ping38/PingTagTask.php
@@ -3,13 +3,18 @@
 namespace Henrydm\Ping38;
 
 use pocketmine\scheduler\Task;
+use Henrydm\Ping38\Ping38;
 
 class PingTagTask extends Task{
 	
-	public function onRun(int $tick) : void {
-		foreach(PingTag::getInstance()->getServer()->getOnlinePlayers() as $player){
-			$tag=PingTag::getInstance()->getConfig()->get("Format");
-			$tag=str_replace("{ping}", $player->getPing(), $tag);
+        public function __construct(Ping38 $plugin) {
+                $this->plugin = $plugin;
+        }
+
+	public function onRun() : void {
+		foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
+			$tag = $this->plugin->getConfig()->get("Format");
+			$tag = str_replace("{ping}", $player->getPing(), $tag);
 			$player->setScoreTag($tag);
 		}
 	}
